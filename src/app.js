a9ce2f7b30b1d68a005986c084c71fa19fb6a41f
maka y <nord2sudjp@gmail.com>
2020-04-13 20:50:23 +0900

    link chat app

--- src/app.js
+++ src/app.js
@@ -10,6 +10,8 @@ const viewPath = path.join(__dirname, "../templates/views");
 const partialPath = path.join(__dirname, "../templates/partials");
 
 const port = process.env.PORT || 3000;
+const chat_app_url = process.env.CHAT_APP_URL;
+console.log(process.env.CHAT_APP_URL);
 
 app.set("view engine", "hbs");
 app.set("views", viewPath);
@@ -18,19 +20,27 @@ hbs.registerPartials(partialPath);
 app.use(express.static(publicDirectoryPath));
 
 app.get("", (req, res) => {
-  res.render("index", { title: "Node Portfolio by n2sj", name: "n2sj" });
+  res.render("index", {
+    title: "Node Portfolio by n2sj",
+    chat_app_url,
+    name: "n2sj",
+  });
 });
 
 app.get("/about", (req, res) => {
-  res.render("about", { title: "About", name: "n2sj" });
+  res.render("about", { title: "About", chat_app_url, name: "n2sj" });
 });
 
 app.get("/help", (req, res) => {
-  res.render("help", { title: "Help", name: "n2sj" });
+  res.render("help", { title: "Help", chat_app_url, name: "n2sj" });
 });
 
 app.get("/weather_index", (req, res) => {
-  res.render("weather_index", { title: "Weather Search", name: "n2sj" });
+  res.render("weather_index", {
+    title: "Weather Search",
+    chat_app_url,
+    name: "n2sj",
+  });
 });
 
 app.get("/weather", (req, res) => {
